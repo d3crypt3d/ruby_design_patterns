d39f98ca19f0f973545d9019e108c7f9841a33ee
John Doe <max.s32@i.ua>
2015-12-17 12:53:56 +0200

    Implement the facade pattern: add the tuner class

---
+++ facade/subsystems/Tuner.rb
@@ -0,0 +1,29 @@
+class Tuner
+  def initialize(description, amplifier)
+    @description, @amplifier = description, amplifier)
+  end
+
+  def on
+    "#{@description} is on"
+  end
+
+  def off
+    "#{@description} is off"
+  end
+
+  def set_frequency(frequency)
+    "#{@description} setting frequency to #{frequency}"
+    @frequency = frequency
+  end
+
+  def set_am
+    "#{@description} setting AM mode"
+  end
+
+  def set_fm
+    "#{@description} setting FM mode"
+  end
+
+  privare
+  attr_reader :frequency
+end
